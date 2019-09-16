663dcec792d51c85816a04762e3885137720ed63
ranitachi <fachran.nazarullah@gmail.com>
2019-09-16 06:25:28 +0000

    user details implemented

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'identitas', 'noidentitas', 'imgurl' ,'password',
+        'name','orgcode','faculty','studyprog','eduprog','email', 'identitas', 'noidentitas', 'imgurl' ,'password',
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Http\Request;
 */
 Route::post('plogin', 'API\UserController@login');
 Route::post('pregister', 'API\UserController@register');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('details', 'API\UserController@details');
+    });
