9f0436bba2ea009ef69497720652133c07c20e02
ranitachi <fachran.nazarullah@gmail.com>
2019-09-18 13:20:14 +0000

    backup commit before adding sort

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -32,7 +32,8 @@ public $successStatus = 200;/**
         $validator = Validator::make($request->all(), [ 
             'name' => 'required', 
             'email' => 'required|email', 
-            'password' => 'required', 
+            'password' => 'required',
+         //   'noidentitas' => 'required', 
             'c_password' => 'required|same:password', 
         ]);
 if ($validator->fails()) { 
@@ -43,6 +44,7 @@ $input = $request->all();
         $user = User::create($input); 
         $success['token'] =  $user->createToken('MyApp')-> accessToken; 
         $success['name'] =  $user->name;
+       // $success['noidentitas'] =  $user->noidentitas;
 return response()->json(['success'=>$success], $this-> successStatus); 
     }
 /** 
@@ -53,6 +55,6 @@ return response()->json(['success'=>$success], $this-> successStatus);
     public function details() 
     { 
         $user = Auth::user(); 
-        return response()->json(['success' => $user], $this-> successStatus); 
+        return response()->json(['message' => 'Authenticated', 'success' => $user], $this-> successStatus); 
     } 
 }
