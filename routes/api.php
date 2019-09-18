6cc07645312bb716fe5bbe7fbb98af4a5c687c7e
ranitachi <fachran.nazarullah@gmail.com>
2019-09-18 13:23:03 +0000

    -

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,10 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::get('keluhan/all', 'API\ComplainController@laporanall');
     Route::get('keluhan/sort/status/{name}', 'API\ComplainController@laporanstatus'); //otw
     Route::post('keluhan/sort/{id}', 'API\ComplainController@laporanbycat'); //otw
-    Route::post('details', 'API\UserController@details');
-    Route::get('/amialive', function () {
-        return ['data' => 'yes you are']; //check if token valid
-    });
+    Route::post('amialive', 'API\UserController@details');
+    // Route::get('/amialive', function () {
+    //     return ['data' => 'yes you are']; //check if token valid
+    // });
     Route::post('keluhan/baru', '\Kordy\Ticketit\Controllers\TicketsController@jsonstore');
     Route::get('keluhan/delete/{id}', '\Kordy\Ticketit\Controllers\TicketsController@jsondelete');
     Route::get('showkeluhan/{id}', '\Kordy\Ticketit\Controllers\TicketsController@jsonshow');
